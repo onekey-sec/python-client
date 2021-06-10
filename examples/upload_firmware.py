190f8808ba9f5d3a4f174cf462e4830a05e47395
Marton Illes <marton@iot-inspector.com>
2021-06-10 15:49:03 +0200

    Added example script to upload firmware

---
+++ examples/upload_firmware.py
@@ -0,0 +1,45 @@
+import sys
+
+from getpass import getpass
+from pathlib import Path
+
+from iot_inspector_client import Client
+from iot_inspector_client.models import FirmwareMetadata
+
+API_URL = "https://demo.iot-inspector.com/api"
+EMAIL = sys.argv[1]
+PASSWORD = getpass()
+
+print("Login to IoT Inspector", EMAIL, "@", API_URL)
+client = Client(API_URL)
+client.login(EMAIL, PASSWORD)
+
+tenants = client.get_all_tenants()
+# Pick the first one
+tenant = tenants[0]
+print("Using tenant:", tenant.name)
+client.use_tenant(tenant)
+
+GET_ALL_PRODUCT_GROUP_IDS = """
+{
+  allProductGroups {
+    id
+    name
+  }
+}
+"""
+
+response = client.query(GET_ALL_PRODUCT_GROUP_IDS)
+product_group_ids = [pg["id"] for pg in response["allProductGroups"]]
+
+
+metadata = FirmwareMetadata(
+    name="myFirmware",
+    vendor_name="myVendor",
+    product_name="myProduct",
+    product_group_id=product_group_ids[0],
+)
+
+firmware_path = Path(sys.argv[2])
+res = client.upload_firmware(metadata, firmware_path, enable_monitoring=True)
+print(res)
